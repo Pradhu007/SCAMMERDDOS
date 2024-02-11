2e3cde076cfba70e8c97567041b0e13299c21831
Pradhu007 <55123592+Pradhu007@users.noreply.github.com>
2024-02-11 23:25:16 +1100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import string
 import threading
 import requests
 
+# USE A VPN TO STAY UNDETECTED
 
 # HOW TO FIGHT AGAINST SCAMM SIGN UP PAGES 
 # IDENTIFY THE REQUEST URL BY CLICKING SUBMIT BUTTON ON THE FORM AND going to Headers tab and replace that post url request with your one
@@ -10,9 +11,9 @@ import requests
 # Replace your params with my one along with the following data like username is the param and data is john
 # and then POST using request.post
 def spam(username,password):
-  url = "https://example.com/"
+  url = "https://example.com/" # replace this your scam website's POST URL
   params = {
-    'username': '{}'.format(username),
+    'username': '{}'.format(username), # these fields need to be replaced with your own
     'password': '{}'.format(password),
 
   }
@@ -28,6 +29,7 @@ def mainspam():
  randomusername = ""
  randompassword = ""
  while True:
+     # the implementation of the for loops can be changed for your needs, right now it is designed to generate 10 character username and passwords. 
      for i in range(10):
         randomusername = randomusername + random.choice(string.ascii_letters)
 
@@ -40,6 +42,9 @@ def mainspam():
      spam(randomusername,randompassword)
      randomusername = ""
      randompassword = ""
+
+# these threads can be left alone or changed depending on your needs. rememeber to divide your total num of threads by 2 and split up
+# example, currently the total num of threads is 200, so divide by 2 is 100, so do 2 seperate for loops with value 100 in both of them. 
 
 threads = []
 for i in range(200):
